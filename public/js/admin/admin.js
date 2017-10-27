9b2efc4df73a65da203d3854693f32620b2abf9f
Polo Huang <polo_07@163.com>
2017-10-27 17:11:35 +0800

    添加了bannerform。

--- public/js/admin/admin.js
+++ public/js/admin/admin.js
@@ -12,4 +12,49 @@ $(document).ready(function(){
             $(this).parent().parent().find('.panel_form').css('display', 'block');
         }
     });
+    $('.form-radio').click(function(){
+        radio = $(this);
+        if ($(this).find('input[type=radio]').attr("checked")) {
+            radio_style(1, radio);
+            $(this).find('input[type=radio]').removeAttr("checked");
+        } else {
+            radio_style(0, radio);
+            $(this).parent().find('input[type=radio]').removeAttr("checked");
+            $(this).find('input[type=radio]').attr("checked","checked");
+        }
+    });
+
+
+    $('.file_image').find('input[type=file]').change(function(){
+        console.log($(this));
+        var file_image = $(this).files;
+        $(this).parent().find('span').text(file_image[0].name);
+    });
+    $(".a-upload").on("change","input[type='file']",function(){
+    var filePath=$(this).val();
+    if(filePath.indexOf("jpg")!=-1 || filePath.indexOf("png")!=-1){
+        $(".fileerrorTip").html("").hide();
+        var arr=filePath.split('\\');
+        var fileName=arr[arr.length-1];
+        $(".showFileName").html(fileName);
+    }else{
+        $(".showFileName").html("");
+        $(".fileerrorTip").html("您未上传文件，或者您上传文件类型有误！").show();
+        return false 
+    }
+})
 });
+
+function radio_style(check, radio){
+    // console.log(radio.hasClass('radio-number'));
+    if (check) {
+        if (radio.hasClass('radio-number')) {
+            radio.find('span').css('background-color', '#B1BBBE');
+        }
+    } else {
+        if (radio.hasClass('radio-number')) {
+            radio.parent().find('span').css('background-color', '#B1BBBE');
+            radio.find('span').css('background-color', '#637176');
+        }
+    }
+}
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -26,11 +26,12 @@
             <div class="panel-body panel_form">
                 <form name="system" method="post" action="{{ url('/admin/editSystem') }}">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                    <dl>
-                        <dd>站名&nbsp;Site Name&nbsp;:&nbsp;&nbsp;
+                    <div class="form-group row">
+                        <label for="name" class="col-sm-3 input-label">站名&nbsp;Site Name&nbsp;<span class="color-must">*</span></label>
+                        <div class="col-sm-9">
                             <input type="text" name="name" value="{{ $system->name }}" class="form-input">
-                        </dd>
-                    </dl>
+                        </div>
+                    </div>
                     <input type="submit" value="提交" class="form-submit">
                 </form>
             </div>
@@ -52,15 +53,54 @@
                     @foreach($banners as $key => $value)
                     <tr>
                         <td>{{ $key + 1 }}</td>
-                        <td><img src="{{ url('uploads/banners/'.$value->path) }}"></td>
+                        <td><a href="{{ url('uploads/banners/'.$value->path) }}" target="_block"><img src="{{ url('uploads/banners/'.$value->path) }}"></a></td>
                         <td>{{ $value->position }}</td>
-                        <td><a class="btn-cool btn-one" href="">Edit</a>&nbsp;<a class="btn-cool btn-one" href="">delete</a></td>
+                        <td><a class="" href=""><i class="fa fa-edit"></i></a>&nbsp;&nbsp;&nbsp;<a class="" href=""><i class="fa fa-trash-o"></i></a></td>
                     </tr>
                     @endforeach
                 </table>
             </div>
-            <div class="panel-body panel_form">
-                add
+            <div class="panel-body ">
+                <form name="system" method="post" action="{{ url('/admin/bannerForm') }}">
+                    <div class="form-group row">
+                        <label for="path" class="col-sm-3 input-label">横图&nbsp;Banner&nbsp;<span class="color-must">*</span></label>
+                        <div class="col-sm-9">
+                            <!-- <span class="form-file file-image btn-cool btn-two">
+                                <span>upload</span>
+                                <input type="file" name="path">
+                            </span> -->
+                            <a href="javascript:;" class="a-upload">
+                                <input type="file" name="" id="">点击这里上传文件
+                            </a>
+
+                            <a href="javascript:;" class="file">选择文件
+                                <input type="file" name="" id="">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="position" class="col-sm-3 input-label">位置&nbsp;Position&nbsp;<span class="color-must">*</span></label>
+                        <div class="col-sm-9">
+                            <div class="form-radio radio-number">
+                                <input type="radio" name="position" value="1">
+                                <span>1</span>
+                            </div>
+                            <div class="form-radio radio-number">
+                                <input type="radio" name="position" value="2">
+                                <span>2</span>
+                            </div>
+                            <div class="form-radio radio-number">
+                                <input type="radio" name="position" value="3">
+                                <span>3</span>
+                            </div>
+                            <div class="form-radio radio-number">
+                                <input type="radio" name="position" value="4">
+                                <span>4</span>
+                            </div>
+                        </div>
+                    </div>
+                    <input type="submit" value="提交" class="form-submit">
+                </form>
             </div>
         </div>
 
